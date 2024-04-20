51b43554fa0f4fadd58ca11fdbee770ef8dd77ad
Zeta <66399177+Zet-a@users.noreply.github.com>
2024-04-20 13:38:01 -0500

    Update Utility.lua

--- Utility.lua
+++ Utility.lua
@@ -35,9 +35,17 @@ function library:AddChams(identifier: string, part: BasePart | Model, color: Col
     local partClone = part:Clone()
     partClone.Parent = library:ChamsContainer(identifier, color)
     if partClone:IsA("BasePart") then
+        if partClone:FindFirstChildWhichIsA("ProximityPrompt",true) then
+            local prox = partClone:FindFirstChildWhichIsA("ProximityPrompt",true)
+            local origprox = part:FindFirstChildWhichIsA("ProximityPrompt",true)
+            prox.InputHoldBegan:Connect(function()
+                fireproximityprompt(origprox)
+            end
+        end
         partClone.Transparency = 0.99
         partClone.CanCollide = false
         partClone.Anchored = true
+        partClone.Position = part.Position
     end
     part.Destroying:Connect(function() partClone:Destroy() end)
 end
